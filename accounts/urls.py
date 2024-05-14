ca7df912004800e11602d271ff420482e98df133
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-05-14 11:35:11 +0300

    Fit: create access and refresh cookies

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path, include
+from .views import (
+    CustomTokenObtainPairView,
+    CustomTokenRefreshView,
+    CustomTokenVerifyView,
+    LogoutView
+)
 
 urlpatterns = [
     path('', include('djoser.urls')),
-    path('', include('djoser.urls.jwt')),
+    
+    path('jwt/create/', CustomTokenObtainPairView.as_view()),
+    path('jwt/refresh/', CustomTokenRefreshView.as_view()),
+    path('jwt/verify/', CustomTokenVerifyView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
