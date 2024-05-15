d3064276d6966f19effe197d5b8a526035439e8f
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-05-15 20:46:34 +0300

    feat(auth): add sign in with google

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,21 +1,15 @@
-from django.urls import path, include
-
-from .views import (
-    CustomTokenObtainPairView,
-    CustomTokenRefreshView,
-    CustomTokenVerifyView,
-    LogoutView
-)
+from django.urls import path, re_path
+from .views import (CustomProviderAuthView,
+                    CustomTokenObtainPairView, 
+                    CustomTokenRefreshView,
+                    CustomTokenVerifyView, 
+                    LogoutView
+                    )
 
 urlpatterns = [
-    # Include the URLs from the 'djoser' app
-    path('', include('djoser.urls')),
-    
-    # Custom JWT token endpoints
-    path('jwt/create/', CustomTokenObtainPairView.as_view()),  # Endpoint to obtain JWT token pair
-    path('jwt/refresh/', CustomTokenRefreshView.as_view()),  # Endpoint to refresh JWT token
-    path('jwt/verify/', CustomTokenVerifyView.as_view()),  # Endpoint to verify JWT token
-    
-    # Logout endpoint
-    path('logout/', LogoutView.as_view()),  # Endpoint to log out user
+    re_path(r'^o/(?P<provider>\S+)/$', CustomProviderAuthView.as_view(), name='provider-auth'),
+    path('jwt/create/', CustomTokenObtainPairView.as_view()),
+    path('jwt/refresh/', CustomTokenRefreshView.as_view()),
+    path('jwt/verify/', CustomTokenVerifyView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
