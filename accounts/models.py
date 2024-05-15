b45a94784cc9e9b97e6efec14dcd4462723420fe
Macharia-Maurice <151857603+Macharia-Maurice@users.noreply.github.com>
2024-05-15 10:02:03 +0300

    cookies feature (#4)
    * Fit: create access and refresh cookies
    
    * add environment variables content
    
    * move myUserManager
    
    * feat(cookies) added comments to cookies setup
    
    ---------
    
    Co-authored-by: Joseph Ngure <121125169+ngure1@users.noreply.github.com>

--- accounts/models.py
+++ accounts/models.py
@@ -1,53 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser ,PermissionsMixin
+from django.contrib.auth.models import  AbstractBaseUser ,PermissionsMixin
+from . import managers
 
+# custom user model
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model representing a user of the application.
+    """
 
-class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None , **kwargs):
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        email=self.normalize_email(email)
-
-        user = self.model(
-            email = email.lower(),
-            password = password,
-            **kwargs,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email,  password=None , **kwargs):
-        
-        user = self.create_user(
-            email,
-            password=password,
-            **kwargs
-        )
-
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-
-
-class MyUser(AbstractBaseUser,PermissionsMixin):
-    email = models.EmailField(max_length=255,unique=True,)
+    email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
-
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
-
-    objects = MyUserManager()
+    objects = managers.MyUserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name","last_name"]
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
+        """
+        Returns a string representation of the user.
+        """
         return self.email
