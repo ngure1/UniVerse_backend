9cc72f3cc545a936392d5679fed4edb0a68d39a9
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-06-03 08:35:17 +0300

    chore: Add API schema endpoints for Swagger and Redoc documentation

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,16 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('accounts.urls')),
     path('post/', include("posts.urls")),
-    path('support/', include('support.urls'))
+    path('support/', include('support.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
