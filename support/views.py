cc180002537d1ae36d04b285b3783a2141f3d100
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-06-03 08:34:58 +0300

    chore: Import ValidationError from rest_framework.exceptions

--- support/views.py
+++ support/views.py
@@ -7,6 +7,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.exceptions import ValidationError
 
 
 # Create your views here.
