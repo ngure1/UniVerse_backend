984131ec288a9bd81ebab48854a2ee6276124d4f
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-06-05 12:03:34 +0300

    chore: Update posts URLs and add endpoints for comments, likes, and bookmarks

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,14 @@ from . import views
 from . import models
 urlpatterns = [
     path("list/", views.ListCreatePosts.as_view()),
-    path("detail/<int:pk>/", views.PostsDetail.as_view()),
+    path("detail/<int:pk>/", views.PostsDetail.as_view(),name="post-detail"),
     path('user/<int:user_id>/posts/', views.UserPostsList.as_view(), name='user-posts-list'),
-    # path("posts/", views.postList),
-    # path("posts/<int:pk>/", views.postDetail),
-    path("search/", views.search_posts),
+    path('<int:post_id>/comments/count/', views.PostCommentsCount.as_view(), name='post-comments-count'),
+    path('<int:post_id>/comments/', views.PostCommentsList.as_view(), name='post-comments-list'),
+    path('<int:post_id>/likes/count/', views.PostLikesCount.as_view(), name='post-likes-count'),
+    path('<int:post_id>/likes/', views.PostLikesList.as_view(), name='post-likes-list'),
+    path('<int:post_id>/bookmarks/count/', views.PostBookmarksCount.as_view(), name='post-bookmarks-count'),
+    path('<int:post_id>/bookmarks/', views.PostBookmarksList.as_view(), name='post-bookmarks-list'),
+
+    path("search/", views.SearchPosts.as_view()),
 ]
