631a62f8ef39380970136ca0e7b5918b2ce4560b
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-06-09 22:37:10 +0300

    🔥 remove: Update Announcement views to use consistent formatting and remove unused imports

--- announcements/views.py
+++ announcements/views.py
@@ -1,33 +1,32 @@
-from django.shortcuts import render
+
 from .models import Announcement
 from .serializers import AnnouncementSerializer
 from rest_framework import status
-from rest_framework.response import Response  
-from rest_framework import generics 
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from posts.permissions import IsOwnerOrReadOnly
 from accounts.models import UserProfile
 from django.db.models import Q
 from support.permissions import IsAdminOrReadOnly
-from django.utils.translation import gettext_lazy as _  
+from django.utils.translation import gettext_lazy as _
 
 # Create your views here.
 class ListCreateAnnouncement(generics.ListCreateAPIView):
-    queryset = Announcement.objects.all()  
-    serializer_class = AnnouncementSerializer  
+    queryset = Announcement.objects.all()
+    serializer_class = AnnouncementSerializer
     permission_classes = [IsAdminOrReadOnly]
 
     def perform_create(self, serializer):
         user_profile = self.request.user.user_profile
         if not self.request.user.is_superuser:
             raise ValidationError(_("Only admins can create announcements."))
-        serializer.save(creator=user_profile) 
+        serializer.save(creator=user_profile)
 
 
 class AnnouncementDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Announcement.objects.all()  
-    serializer_class = AnnouncementSerializer  
+    queryset = Announcement.objects.all()
+    serializer_class = AnnouncementSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
 
@@ -41,10 +40,10 @@ class AnnouncementSearchView(generics.GenericAPIView):
         if not query:
             return Response({'error': _('Query parameter "q" is required.')}, status=status.HTTP_400_BAD_REQUEST)
 
-        announcement_results = Announcement.objects.filter(  
-            Q(title__icontains=query) | 
+        announcement_results = Announcement.objects.filter(
+            Q(title__icontains=query) |
             Q(creator__user__email__icontains=query) |
-            Q(creator__user__first_name__icontains=query) |  
+            Q(creator__user__first_name__icontains=query) |
             Q(content__icontains=query)
         )
 
