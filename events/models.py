0420d5f7f25b7b6ef492240849bb3f002822429b
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-06-09 23:35:22 +0300

    🔥 refactor: Remove redundant code and reorder fields in Event model

--- events/models.py
+++ events/models.py
@@ -7,9 +7,6 @@ class Event(models.Model):
     author = models.ForeignKey("accounts.UserProfile",on_delete=models.CASCADE, related_name="Event")
     title = models.CharField(max_length=255, default="Default Title")
     description = models.TextField(_("Description"), blank=False)
-    created_at = models.DateTimeField(_("Date Created"),auto_now_add=True)
-    updated_at = models.DateTimeField(_("Updated_at"),auto_now=True)
-    # address = models.OneToOneField("accounts.Address", on_delete=models.CASCADE)
     media = models.FileField(
     _("Event Media"),
     null=True, blank=True,
@@ -17,6 +14,9 @@ class Event(models.Model):
     validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'gif', 'mp4', 'mov', 'avi'])]
     )
     capacity = models.IntegerField(_("Event's capacity"))
+    created_at = models.DateTimeField(_("Date Created"),auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated_at"),auto_now=True)
+    # address = models.OneToOneField("accounts.Address", on_delete=models.CASCADE)
 
     
 
