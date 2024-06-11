1ff0df99e9607648f3101a2952595ae564c707fc
theurikarue <iambonitheuri@gmail.com>
2024-06-11 05:38:02 +0300

    🔨 refactor: Update URLs and add new endpoints for profiles, searching, following, and followers

--- accounts/urls.py
+++ accounts/urls.py
@@ -26,19 +26,21 @@ urlpatterns = [
     # Include the URLs from the 'djoser' app
     path('auth/', include('djoser.urls')),
     re_path(r'^o/(?P<provider>\S+)/$', CustomProviderAuthView.as_view(), name='provider-auth'),
+    
     # Custom JWT token endpoints
     path('auth/jwt/create/', CustomTokenObtainPairView.as_view()),  # Endpoint to obtain JWT token pair
     path('auth/jwt/refresh/', CustomTokenRefreshView.as_view()),  # Endpoint to refresh JWT token
     path('auth/jwt/verify/', CustomTokenVerifyView.as_view()),  # Endpoint to verify JWT token
   
-    # Logout endpoint
+
     path('auth/logout/', LogoutView.as_view()),  # Endpoint to log out user
-    path('profilelist/', ListProfile.as_view()),
-    path('Educationlist/', ListProfile.as_view()),
-    path('accounts/search/', SearchView.as_view() ),
-    path('follow-toggle/', FollowToggleView.as_view()),
-    path('followers/', FollowerList.as_view()),
-    path('following/', FollowingList.as_view()),
+    path('profile/listcreate/', ListProfile.as_view()),  # List and create profiles
+    path('accounts/search/', SearchView.as_view() ), # Searching for Accounts
+
+    # follow Urls
+    path('follow-toggle/', FollowToggleView.as_view()), #Endpoint for following and unfollowing users
+    path('followers/', FollowerList.as_view()), # Endpoint to list followers
+    path('following/', FollowingList.as_view()), # Ednpoint to list following
 
         # LoggedInUser URLs
     path('profile/<int:pk>/', ProfileDetail.as_view()),  # View and update the logged-in user's profile
