aeedefa4731cd6d7921a2314ee310e4531d0ad01
theurikarue <iambonitheuri@gmail.com>
2024-06-25 07:37:28 +0300

    ♻ - Update the URL pattern for creating support instances - Update the view class for creating and listing support instances

--- support/urls.py
+++ support/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("create/", views.ListCreateSupport.as_view()),
+    path("", views.ListCreateSupport.as_view()),
     path("detail/<int:pk>/", views.SupportDetail.as_view(), name="support-detail"),
     path("search/", views.SupportSearchView.as_view()),
 ]
