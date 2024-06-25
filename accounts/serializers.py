6131525874e8b78c76d4a102133680c3bdfeedbb
theurikarue <iambonitheuri@gmail.com>
2024-06-25 07:11:00 +0300

    ♻ refactor: Add comments in Accounts Serializers and Accounts Views.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -41,9 +41,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ('created_at', 'updated_at', 'followers_count', 'following_count')
     
+    #Define a method to retrieve the followers_count for the followers_count serializermethodfield
     def get_followers_count(self, obj):
         return obj.followers.count()
 
+    #Define a method to retrieve the following_count for the following_count serializermethodfield
     def get_following_count(self, obj):
         return obj.following.count()
 
@@ -69,18 +71,22 @@ class FollowerSerializer(serializers.ModelSerializer):
         model = Follower
         fields = ['id','follower_name','followed_name','created_at','follower_url','followed_url']
 
+    #Define a method to retrieve the follower_name for the follower_name serializermethodfield
     def get_follower_name(self, obj):
         return f"{obj.follower.user.first_name} {obj.follower.user.last_name}"
 
+    #Define a method to retrieve the followed_name for followed_name serializermethodfield
     def get_followed_name(self, obj):
         return f"{obj.followed.user.first_name} {obj.followed.user.last_name}"
     
+    #Define a method to retrieve the follower_url for the follower_url serializermethodfield
     def get_follower_url(self, obj):
         request = self.context.get('request')
         if obj.follower and obj.follower.pk:
             return request.build_absolute_uri(obj.follower.get_absolute_url())
         return None
 
+    #Define a method to retrieve the followed_url for the followed_url serializermethodfield
     def get_followed_url(self, obj):
         request = self.context.get('request')
         if obj.followed and obj.followed.pk:
