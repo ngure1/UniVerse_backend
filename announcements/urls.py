fdbc7af7dc681b1c10f625a3aaca2ac4b04649d8
theurikarue <iambonitheuri@gmail.com>
2024-06-25 07:39:59 +0300

    ♻ Refactor URL pattern for creating support instances

--- announcements/urls.py
+++ announcements/urls.py
@@ -5,7 +5,7 @@ from . import models
 
 #urlpatterns for Announcements app
 urlpatterns = [
-    path("listcreate/", views.ListCreateAnnouncement.as_view()),
+    path("", views.ListCreateAnnouncement.as_view()),
     path("detail/<int:pk>/", views.AnnouncementDetail.as_view(), name="announcement-detail"),
     path("search/", views.AnnouncementSearchView.as_view())
 ]
