5ec403551ab6a558b6cd5361fbb7b85e4d1f62b8
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-06-25 11:25:43 +0300

    ♻️ refactor: Add address and venue fields to Event model

--- events/models.py
+++ events/models.py
@@ -4,21 +4,30 @@ from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 class Event(models.Model):
-    author = models.ForeignKey("accounts.UserProfile",on_delete=models.CASCADE, related_name="Event")
+    is_online = models.BooleanField(_("Is Online"), default=False)
+    is_physical = models.BooleanField(_("Is Physical"), default=False)
+    author = models.ForeignKey("accounts.UserProfile", on_delete=models.CASCADE, related_name="Event")
     title = models.CharField(max_length=255, default="Default Title")
     description = models.TextField(_("Description"), blank=False)
-    address = models.OneToOneField("accounts.Address", on_delete=models.CASCADE, related_name="event_address", null=True, blank=True)
+    address = models.CharField(_("Address"), max_length=255, blank=True, null=True)
+    venue= models.CharField(_("Venue"), max_length=255, blank=True, null=True)
     media = models.FileField(
     _("Event Media"),
     null=True, blank=True,
     upload_to="media/event",
     validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'gif', 'mp4', 'mov', 'avi'])]
     )
-    capacity = models.IntegerField(_("Event's capacity"))
+    event_form_url = models.URLField(_("Event Form URL"), max_length=255, blank=True, null=True)
+    event_start_date = models.DateField(_("Event Start Date"), blank=True, null=True)
+    event_start_time = models.TimeField(_("Event Start Time"), blank=True, null=True)
+    event_end_date = models.DateField(_("Event End Date"), blank=True, null=True)
+    event_end_time = models.TimeField(_("Event End Time"), blank=True, null=True)
+    like= models.ForeignKey("posts.Like", on_delete=models.SET_NULL, related_name="event_likes", blank=True, null=True)
+    comment= models.ForeignKey("posts.Comment", on_delete=models.SET_NULL , related_name="event_comments", blank=True, null=True)
+    bookmark= models.ForeignKey("posts.Bookmark", on_delete=models.SET_NULL,related_name="event_bookmarks", blank=True, null=True)
     created_at = models.DateTimeField(_("Date Created"),auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated_at"),auto_now=True)
 
-    
 
     def __str__(self):
         return self.title
